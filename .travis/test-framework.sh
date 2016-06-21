f1bb016af242549a316c9cf4f0b73ea0de3b340a
Cajus Pollmeier <cajus@naasa.net>
2016-06-21 10:18:34 +0200

    Added more lint checkgs for tests and api

--- .travis/test-framework.sh
+++ .travis/test-framework.sh
@@ -16,6 +16,20 @@ if [ "${QXBROWSER}" = "Firefox" ] && [ "${QXVERSION}" = "latest" ]; then
         exit 1
     fi
 
+    echo "Checking API..."
+    ./generate.py -sI api 2>&1 | grep 'Error:\|Warning:'
+    if [ $? -eq 0 ]; then
+        echo "Documentation warnings are treated as errors! Please fix!"
+        exit 1
+    fi
+
+    echo "Checking tests..."
+    ./generate.py -sI test-source 2>&1 | grep 'Error:\|Warning:'
+    if [ $? -eq 0 ]; then
+        echo "Lint warnings are treated as errors! Please fix!"
+        exit 1
+    fi
+
     exit $RES
 fi
 
