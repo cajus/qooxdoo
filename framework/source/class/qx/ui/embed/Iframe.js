5e6c67e1d6be0deae5322a3a61275163136b58d1
Cajus Pollmeier <pollmeier@gonicus.de>
2017-10-18 14:40:42 +0200

    Added workaround

--- framework/source/class/qx/ui/embed/Iframe.js
+++ framework/source/class/qx/ui/embed/Iframe.js
@@ -245,6 +245,9 @@ qx.Class.define("qx.ui.embed.Iframe",
      */
     _onIframeLoad : function(e)
     {
+      // Workaround for Chrome: touch DOM element
+      this.getContentElement() && this.getContentElement().getDomElement();
+
       this._applyNativeContextMenu(this.getNativeContextMenu(), null);
       this._applyNativeHelp(this.getNativeHelp(), null);
 
